76027b97d797fb105faa1b28e12ea55720928159
crrlxx <guodong.167@163.com>
2019-01-22 09:33:12 +0800

    Test git commit.

--- bankyy.py
+++ bankyy.py
@@ -18,17 +18,14 @@ import urllib
 import urllib.request
 import openpyxl.workbook as workbook
 
-#reload(sys)
-#sys.setdefaultencoding("utf-8")
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 day_now = datetime.date.today()
 day7_bef = str(datetime.date.today() - datetime.timedelta(7))
-print(day_now, day7_bef)
-
-#ios
-#homepath = '/Users/timyan/Desktop/'
-#windows
 homepath = os.getcwd()
+print("Checking out:", day7_bef, day_now)
+print("Working directory:", homepath)
 wb_out = workbook.Workbook(homepath+"out.xlsx")
 ws_out = wb_out.create_sheet("out")
 wb_out.save("out.xlsx")
@@ -39,6 +36,15 @@ dict_code = {'CYCC000': '国债', 'CYCC021': '政策性金融债（国开）',
              'CYCC41A': '同业存单AAA+', 'CYCC41B': '同业存单AAA', 'CYCC41C': '同业存单AA+',
              'CYCC80A': '企业债AAA+', 'CYCC80B': '企业债AAA', 'CYCC80D': '企业债AA+'}
 
+#国债 CYCC000 0.25 0.5 0.75 1.0 1.5 2.0
+#http://www.chinamoney.com.cn/ags/ms/cm-u-bk-currency/ClsYldCurvHis?lang=CN&bondType=CYCC000&reference=1&startDate=2019-01-13&endDate=2019-01-20&termId=0.5
+#政策性金融债（国开） CYCC021 0.25 0.5 0.75 1.0 1.5 2.0
+#http://www.chinamoney.com.cn/dqs/rest/cm-u-bk-currency/ClsYldCurvHisExcel?lang=CN&bondType=CYCC021&reference=1&startDate=2019-01-13&endDate=2019-01-20&termId=0.5
+#中期票据AA+(CYCC82D) AAA-(CYCC82C) AAA(CYCC82B) AAA+(CYCC82A) 0.25 0.5 0.75 1.0 1.5 2.0
+#http://www.chinamoney.com.cn/dqs/rest/cm-u-bk-currency/ClsYldCurvHisExcel?lang=CN&bondType=CYCC82A&reference=1&startDate=2018-12-18&endDate=2019-01-18&termId=0.5
+#短期融资券 AA+(CYCC81D) AAA-(CYCC81C) AAA(CYCC81B) AAA+(CYCC81A)
+#企业债 AA+(CYCC80D) AAA(CYCC80B) AAA+(CYCC80A)
+#同业存单 AA+(CYCC41C)  AAA(CYCC41B) AAA+(CYCC41A)
 
 def cbk(a,b,c):
     '''''回调函数
@@ -70,26 +76,11 @@ def loadexcel(code):
     try:
         print('Begin download excel:'+dict_code[code])
         #python3
-        urllib.request.urlretrieve(base_url + code + sdate + day7_bef + edate + str(day_now) + end_url, homepath + os.sep + code + '.xlsx', cbk)
+        #urllib.request.urlretrieve(base_url + code + sdate + day7_bef + edate + str(day_now) + end_url, homepath + os.sep + code + '.xlsx', cbk)
         #python2
-        #urllib.urlretrieve(base_url + code + sdate + day7_bef + edate + str(day_now) + end_url, homepath +os.sep+ code + '.xlsx', cbk)
+        urllib.urlretrieve(base_url + code + sdate + day7_bef + edate + str(day_now) + end_url, homepath +os.sep+ code + '.xlsx', cbk)
     except Exception as e:
-        print("Download file failed:")
         print(e)
-
-    # except Exception, e:
-    #    print e
-
-
-#国债 CYCC000 0.25 0.5 0.75 1.0 1.5 2.0
-#http://www.chinamoney.com.cn/ags/ms/cm-u-bk-currency/ClsYldCurvHis?lang=CN&bondType=CYCC000&reference=1&startDate=2019-01-13&endDate=2019-01-20&termId=0.5
-#政策性金融债（国开） CYCC021 0.25 0.5 0.75 1.0 1.5 2.0
-#http://www.chinamoney.com.cn/dqs/rest/cm-u-bk-currency/ClsYldCurvHisExcel?lang=CN&bondType=CYCC021&reference=1&startDate=2019-01-13&endDate=2019-01-20&termId=0.5
-#中期票据AA+(CYCC82D) AAA-(CYCC82C) AAA(CYCC82B) AAA+(CYCC82A) 0.25 0.5 0.75 1.0 1.5 2.0
-#http://www.chinamoney.com.cn/dqs/rest/cm-u-bk-currency/ClsYldCurvHisExcel?lang=CN&bondType=CYCC82A&reference=1&startDate=2018-12-18&endDate=2019-01-18&termId=0.5
-#短期融资券 AA+(CYCC81D) AAA-(CYCC81C) AAA(CYCC81B) AAA+(CYCC81A)
-#企业债 AA+(CYCC80D) AAA(CYCC80B) AAA+(CYCC80A)
-#同业存单 AA+(CYCC41C)  AAA(CYCC41B) AAA+(CYCC41A)
 
 
 if __name__ == '__main__':
