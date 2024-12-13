45a38dcb8bb5e4b1a498459ab03e7b2cb60f2d4b
yangd6 <yangd6@xiaopeng.com>
2024-12-13 10:04:41 +0800

    create new branch

--- entityReplace/util/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2017/5/17 15:18
-# @Author  : crrlxx
-# @contact: @gmail.com
-# @Site    : 
-# @File    : __init__.py.py
-# @Software: PyCharm Community Edition
